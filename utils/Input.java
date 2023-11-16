2a74dc999b29f257359716ac6941eef7c144649e
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-11-16 11:58:15 -0600

    thursday nov 16 commit before lunch

---
+++ utils/Input.java
@@ -0,0 +1,83 @@
+package utils;
+
+import java.util.Scanner;
+
+public class Input {
+    private final Scanner scanner;
+//    private String string;
+
+    public Input() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String getString() {
+        return scanner.nextLine();
+    }
+
+    public String getString(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine();
+    }
+
+    public boolean yesNo() {
+        System.out.println("Do you like stuff?");
+        String userInput = scanner.nextLine();
+        return userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes");
+    }
+
+    public boolean yesNo(String prompt) {
+        System.out.print(prompt);
+        return yesNo();
+    }
+
+    public int getInt(String prompt) {
+        System.out.print(prompt);
+        return getInt();
+    }
+
+    public int getInt(int min, int max) {
+        int userInt;
+        do {
+            System.out.printf("Enter a number between %d and %d: %n", min, max);
+            userInt = Integer.parseInt(scanner.nextLine());
+//            userInt = scanner.nextInt(); //
+        } while (userInt > max || userInt < min);
+        return userInt;
+    }
+
+    public int getInt() {
+        int userInt = 0;
+        String s = getString();
+        try{
+            userInt = Integer.valueOf(s);
+//            userInt = Integer.parseInt(s);
+        } catch(NumberFormatException e){
+            System.out.println("Not an integer. Try again: ");
+            userInt = getInt();
+        }
+        return userInt;
+    }
+    public double getDouble(double min, double max){
+        double userDouble;
+        do{
+            System.out.printf("Enter a number between %f and %f: %n", min, max);
+            userDouble = Double.parseDouble(scanner.nextLine());
+        } while(userDouble > max || userDouble < min);
+        return userDouble;
+    }
+
+    public double getDouble(){
+        double userDub = 0;
+        String s = getString();
+        try {
+            userDub = Double.valueOf(s);
+//            userDub = Double.parseDouble(s);
+        } catch(NumberFormatException e){
+            System.out.println("Wrong double. Try again: ");
+            userDub = getDouble();
+        }
+        return userDub;
+    }
+
+
+}
